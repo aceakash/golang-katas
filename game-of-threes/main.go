00cb51837eb3772347aeb97231ae267be3862567
Akash <akashkurdekar@gmail.com>
2015-11-07 23:57:58 +0000

    working

--- game-of-threes/main.go
+++ game-of-threes/main.go
@@ -5,8 +5,26 @@ type step struct {
 	adjustment int
 }
 
-func getSteps(start int) []int {
-	return make([]int, start)
+func getAdjustment(value int) int {
+	switch value % 3 {
+	case 1:
+		return -1
+	case 2:
+		return 1
+	default:
+		return 0
+	}
+}
+
+func getSteps(start int) []step {
+	currentVal := start
+	steps := []step{}
+	for currentVal > 1 {
+		adjustment := getAdjustment(currentVal)
+		steps = append(steps, step{currentVal, adjustment})
+		currentVal = (currentVal + adjustment) / 3
+	}
+	return append(steps, step{1, 0})
 }
 
 func main() {
--- game-of-threes/main_test.go
+++ game-of-threes/main_test.go
@@ -6,32 +6,53 @@ import (
 	"testing"
 )
 
-type getStepsTestDataItem struct {
+var getStepsTests = []struct {
 	start int
 	steps []step
+}{
+	{1, []step{
+		{1, 0},
+	}},
+	{2, []step{
+		{2, 1},
+		{1, 0},
+	}},
+	{20, []step{
+		{20, 1},
+		{7, -1},
+		{2, 1},
+		{1, 0},
+	}},
 }
 
 func TestGetSteps(t *testing.T) {
-	var getStepsTests = []getStepsTestDataItem{
-		{1, []step{
-			{1, 0},
-		}},
-		{2, []step{
-			{2, 1},
-			{1, 0},
-		}},
-		{20, []step{
-			{20, 1},
-			{7, -1},
-			{2, 1},
-			{1, 0},
-		}},
-	}
+
 	fmt.Println(getStepsTests)
 	for _, tt := range getStepsTests {
 		actual := getSteps(tt.start)
 		if !reflect.DeepEqual(actual, tt.steps) {
 			t.Errorf("getSteps %d was %v, expected %v", tt.start, actual, tt.steps)
+		}
+	}
+}
+
+var getAdjustmentTests = []struct{ v, a int }{
+	{1, -1},
+	{2, 1},
+	{3, 0},
+	{4, -1},
+	{5, 1},
+	{6, 0},
+	{7, -1},
+	{8, 1},
+	{9, 0},
+}
+
+func TestGetAdjustment(t *testing.T) {
+	for _, tt := range getAdjustmentTests {
+		actual := getAdjustment(tt.v)
+		if actual != tt.a {
+			t.Errorf("getAdjustment %d was %d, expected %d", tt.v, actual, tt.a)
 		}
 	}
 }
