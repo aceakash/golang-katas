d377619bc9ece9eacb62754267ef1a9c53b595cc
Akash <akashkurdekar@gmail.com>
2015-11-07 21:35:27 +0000

    some test data

--- game-of-threes/main.go
+++ game-of-threes/main.go
@@ -1,5 +1,10 @@
 package main
 
+type step struct {
+	value      int
+	adjustment int
+}
+
 func getSteps(start int) []int {
 	return make([]int, start)
 }
--- game-of-threes/main_test.go
+++ game-of-threes/main_test.go
@@ -1,22 +1,37 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 type getStepsTestDataItem struct {
 	start int
-	steps []int
-}
-
-var getStepsTests = []getStepsTestDataItem{
-	{1, []int{1}},
-	{2, []int{2, 1}},
+	steps []step
 }
 
 func TestGetSteps(t *testing.T) {
+	var getStepsTests = []getStepsTestDataItem{
+		{1, []step{
+			{1, 0},
+		}},
+		{2, []step{
+			{2, 1},
+			{1, 0},
+		}},
+		{20, []step{
+			{20, 1},
+			{7, -1},
+			{2, 1},
+			{1, 0},
+		}},
+	}
+	fmt.Println(getStepsTests)
 	for _, tt := range getStepsTests {
 		actual := getSteps(tt.start)
-		if len(actual) != len(tt.steps) {
-			t.Errorf("getSteps %d has %d steps, expected %d", tt.start, len(actual), len(tt.steps))
+		if !reflect.DeepEqual(actual, tt.steps) {
+			t.Errorf("getSteps %d was %v, expected %v", tt.start, actual, tt.steps)
 		}
 	}
 }
